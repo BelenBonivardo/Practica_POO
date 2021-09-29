94cbd7e4d7db309e88e8f56a9a081ea3d0996496
BelenBonivardo <84883291+BelenBonivardo@users.noreply.github.com>
2021-09-28 22:21:39 -0300

    Add files via upload
    adicion ejercicios poo

---
+++ Result.java
@@ -0,0 +1,46 @@
+package src.poo;
+
+public class Result{
+public Result(){
+
+}
+private double resultSum;
+        public double getResultSum() {
+        return resultSum;
+
+    }
+        public void setResultSum(double resultSum) {
+        this.resultSum = resultSum;
+    }
+    private double resultSubtraction;
+
+    public double getResultSubtraction() {
+        return resultSubtraction;
+    }
+
+    public void setResultSubtraction(double resultSubtraction) {
+        this.resultSubtraction = resultSubtraction;
+    }
+
+    private double multiplication;
+
+         public double getMultiplication() {
+             return multiplication;
+    }
+
+         public void setMultiplication(double multiplication) {
+            this.multiplication = multiplication;
+    }
+
+    private double division;
+
+         public double getDivision() {
+            return division;
+    }
+
+         public void setDivision(double division) {
+            this.division = division;
+    }
+
+
+}
